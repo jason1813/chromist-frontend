da2cb297ed0ac46ffcccdef3750750dd4bce376d
Jason Morris <jmmorris2@mail.bradley.edu>
2022-05-29 14:56:53 -0500

    Do some of ListThread

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import React from 'react';
+import { View } from 'react';
+import { Text } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +20,74 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <ListThread/>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+class ListThread extends Component {
+  render() {
+    return (
+        <div
+          className="thread"
+          style={{
+            height: 100,
+            width: "100%",
+            position: 'relative',
+            backgroundColor: "white"
+          }}
+        >
+          <h1
+            style={{
+              textAlign: "left",
+              color: "black",
+              verticalAlign: "top",
+              fontSize: "17px",
+              marginRight: "50px",
+              marginLeft: "12px",
+              marginTop: "8px"
+            }}
+          >
+            Billy Napier: Florida Football Wont Get Involved in NIL Bidding Wars
+          </h1>
+
+          <div
+            style={{
+              position: "absolute",
+              bottom: 0,
+              display: "flex"
+            }}
+          >
+            <p
+              style={{
+                textAlign: "left",
+                marginLeft: "12px",
+                marginBottom: "8px",
+                fontSize: "14px",
+                color: "blue"
+              }}
+            >
+              hiwe5qf89q3u4
+            </p>
+            <p
+              style={{
+                textAlign: "left",
+                marginLeft: "12px",
+                marginBottom: "8px",
+                fontSize: "14px",
+                color: "black"
+              }}
+            >
+              45 comments
+            </p>
+          </div>
+
+        </div>
+    )
+  }
+}
