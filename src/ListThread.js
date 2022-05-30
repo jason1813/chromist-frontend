ef817b6bd7510a0fa0f1898b8e79437fd1a8f140
Jason Morris <jmmorris2@mail.bradley.edu>
2022-05-30 14:01:50 -0500

    Put ListThread in its own file

--- src/ListThread.js
+++ src/ListThread.js
@@ -1,69 +1,20 @@
 
+import './ListThread.css';
 import ListThreadVote from './ListThreadVote.js'
 
 function ListThread(props) {
   return (
-    <div
-      className="listThread"
-      style={{
-        height: 100,
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        backgroundColor: "white",
-        justifyContent: 'space-between',
-        marginTop: 4,
-      }}
-    >
-      <div
-        className="textContent"
-        style={{
-          marginLeft: 15,
-          marginTop: 8,
-          marginBottom: 8,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: 'space-between',
-        }}
-      >
-        <h1
-          style={{
-            textAlign: "left",
-            color: "black",
-            fontSize: "17px",
-            marginTop: 0,
-            maxHeight: 60,
-          }}
-        >
+    <div className="ListThread">
+      <div className="TextContent">
+        <h1 className='ListThread-title'>
           {props.title}
         </h1>
 
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <p
-            style={{
-              textAlign: "left",
-              fontSize: "14px",
-              margin: 0,
-              color: "blue"
-            }}
-          >
+        <div className='ListThread-info'>
+          <p className='ListThread-author'>
             {props.author}
           </p>
-          <p
-            style={{
-              textAlign: "left",
-              marginLeft: "12px",
-              marginTop: 0,
-              marginBottom: 0,
-              fontSize: "14px",
-              color: "black"
-            }}
-          >
+          <p className='ListThread-number-of-comments'>
             {props.numberOfComments} comments
           </p>
         </div>
