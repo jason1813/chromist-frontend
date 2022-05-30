3c8a23d5819d6dc83673f7305b0bf6a5bb62e4b8
Jason Morris <jmmorris2@mail.bradley.edu>
2022-05-30 12:00:18 -0500

    Use css for ListThreadVote

--- src/ListThreadVote.js
+++ src/ListThreadVote.js
@@ -1,49 +1,24 @@
 
+import './ListThreadVote.css';
+
 function ListThreadVote(props) {
   return (
-    <div
-      className="listThreadVote"
-      style={
-        {
-          height: 100,
-          width: 40,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: 'space-around',
-          marginRight: 15,
-          marginLeft: 8,
-        }
-      }
-    >
+    <div className='ListThreadVote'>
 
-      <img src={require('./up-arrow.png')}
-        style={{
-          width: 30,
-          marginTop: 10,
-          marginBottom: 0,
-          marginLeft: "auto",
-          marginRight: "auto",
-        }}
+      <img 
+        className='ListThreadVote-up-arrow'
+        src={require('./up-arrow.png')}
+        alt="up arrow"
       />
 
-      <p style={{
-        color: "black",
-        minWidth: "100%",
-        marginTop: 0,
-        marginBottom: 0,
-        fontSize: 17,
-      }}>
+      <p className='ListThreadVote-upvote-score'>
         {props.upVoteScore}
       </p>
 
-      <img src={require('./down-arrow.png')}
-        style={{
-          width: 30,
-          marginTop: 0,
-          marginBottom: 10,
-          marginLeft: "auto",
-          marginRight: "auto",
-        }}
+      <img 
+        className='ListThreadVote-down-arrow'
+        src={require('./down-arrow.png')}
+        alt="down arrow"
       />
     </div>
   )
