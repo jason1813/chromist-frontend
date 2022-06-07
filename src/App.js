cdf6948b671580f7c38282f1772dfb09da7f1f13
Jason Morris <jmmorris2@mail.bradley.edu>
2022-06-06 21:56:08 -0500

    Setup router

--- src/App.js
+++ src/App.js
@@ -1,51 +1,20 @@
 import './misc/css/App.css';
 import { Component } from 'react';
 import React from 'react';
-import ListThread from './pages/threads/ListThread.js'
-import getThreads from './network/network_calls';
 import NavBar from './misc/js/NavBar.js'
-import BottomBar from './pages/threads/BottomBar.js'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Threads from "./pages/threads/Threads"
 
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      threadData: [],
-    }
-  }
-
-  componentDidMount() {
-    getThreads.then(data => {
-      this.setState({threadData: data})
-      console.log(this.state)
-    })
-  }
-
   render() {
-
-    const threadProps = this.state.threadData.map((threadDataItem) =>
-      <ListThread
-        id={threadDataItem.id}
-        dateCreated={threadDataItem.dateCreated}
-        author={threadDataItem.author}
-        title={threadDataItem.title}
-        description={threadDataItem.description}
-        numberOfComments={threadDataItem.numberOfComments}
-        upvoteScore={threadDataItem.upvoteScore}
-        userUpvoted={threadDataItem.userUpvoted}
-      />
-    )
-
     return (
-      <div className="App">
+      <Router>
         <NavBar isLoggedIn={false} />
-        <div className='threads'>
-          {threadProps}
-        </div>
-        <BottomBar isNext={true} isPrevious={true} />
-      </div>
+        <Routes>
+          <Route path="/" element={<Threads />} />
+        </Routes>
+      </Router>
     )
   }
 }
---
+++ src/pages/threads/Threads.js
@@ -0,0 +1,50 @@
+// import './Threads.css';
+import { Component } from 'react';
+import React from 'react';
+import ListThread from './ListThread.js'
+import getThreads from '../../network/network_calls';
+import BottomBar from './BottomBar.js'
+
+class Threads extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            threadData: [],
+        }
+    }
+
+    componentDidMount() {
+        getThreads.then(data => {
+            this.setState({ threadData: data })
+            console.log(this.state)
+        })
+    }
+
+    render() {
+
+        const threadProps = this.state.threadData.map((threadDataItem) =>
+            <ListThread
+                id={threadDataItem.id}
+                dateCreated={threadDataItem.dateCreated}
+                author={threadDataItem.author}
+                title={threadDataItem.title}
+                description={threadDataItem.description}
+                numberOfComments={threadDataItem.numberOfComments}
+                upvoteScore={threadDataItem.upvoteScore}
+                userUpvoted={threadDataItem.userUpvoted}
+            />
+        )
+
+        return (
+            <div className="threads">
+                <div className='threads-list'>
+                    {threadProps}
+                </div>
+                <BottomBar isNext={true} isPrevious={true} />
+            </div>
+        )
+    }
+}
+
+export default Threads;
