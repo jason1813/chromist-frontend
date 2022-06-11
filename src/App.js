e576223d7286b475805bced59c158823ff929bfe
Jason Morris <jmmorris2@mail.bradley.edu>
2022-06-11 16:07:15 -0500

    Add line descendent (still needs more formatting

--- src/App.js
+++ src/App.js
@@ -15,6 +15,21 @@ class App extends Component {
 
     document.body.style.backgroundColor = "var(--oxford)";
 
+    let replies = [
+      {
+        author: "biblemania34",
+        text: "Lorem ipsum dolor sit amet, alii rebum postea eam ex. Et mei laoreet officiis, summo sensibus id mei.",
+        userUpvoted: "up",
+        upvoteScore: "543"
+      },
+      {
+        author: "biblfdia34",
+        text: "Lorem ipsum dolor sit amet, alii rebum postea eam ex. Et mei laoreet officiis, summo sensibus id mei.",
+        userUpvoted: "up",
+        upvoteScore: "543"
+      }
+    ]
+
     return (
       <Router>
         <NavBar isLoggedIn={false} />
@@ -24,11 +39,12 @@ class App extends Component {
             {/* <Route path="/" element={<ThreadDetailTop />} /> */}
             {/* <Route path="/" element={<ThreadDetail title='Is the Holy Trinity Biblical?' description="Is the Trinity a biblical concept?Come to think of it, I have never seen it mentioned in the Bible. Jesus speaks multiple times of how the he does the will of the Father in heaven and there are multiple times Jesus prays to God as well.Yes Jesus is our savior, but if he was God he would not need to pray to anyone.There is one God: Deuteronomy 6:4; 1 Corinthians 8:4; Galatians 3:20; 1 Timothy 2:5.I am not underestimating Jesus here. But why would we teach this if it's not in the Bible?" />} /> */}
             {/* <Route path="/" element={<SideBySideVote upvoteScore='4' userUpvoted="up" />} /> */}
-            <Route path="/" element={<Comment 
-            author="biblemania34" 
-            text="Lorem ipsum dolor sit amet, alii rebum postea eam ex. Et mei laoreet officiis, summo sensibus id mei." 
-            userUpvoted="up"
-            upvoteScore="543"
+            <Route path="/" element={<Comment
+              author="biblemania34"
+              text="Lorem ipsum dolor sit amet, alii rebum postea eam ex. Et mei laoreet officiis, summo sensibus id mei."
+              userUpvoted="up"
+              upvoteScore="543"
+              replies={replies}
             />} />
 
           </Routes>
--- src/pages/thread/Comment/Comment.js
+++ src/pages/thread/Comment/Comment.js
@@ -4,17 +4,36 @@ import React from 'react';
 import SideBySideVote from '../SideBySideVote';
 
 function Comment(props) {
+
+    console.log(props.replies)
+
     return (
         <div className="comment">
-            <img
-                className='comment-profile'
-                src={require('../../../misc/img/profile.png')}
-                alt="profile"
-            />
+            <div className='comment-profile-line'>
+                {/* <a href='blah'> */}
+                    <img
+                        className='comment-profile'
+                        src={require('../../../misc/img/profile.png')}
+                        alt="profile"
+                    />
+                    {/* <a href='blah'> */}
+                    <hr className='comment-actual-line' />
+                {/* </a> */}
+            </div>
             <div className='comment-content'>
                 <p className='comment-author'>{props.author}</p>
                 <p className='comment-text'>{props.text}</p>
                 <SideBySideVote userUpvoted={props.userUpvoted} upvoteScore={props.upvoteScore} />
+                {props.replies != undefined && props.replies.length != 0 &&
+                    <div className='replies'>
+                        <Comment
+                            author={props.replies[0].author}
+                            text={props.replies[0].text}
+                            userUpvoted={props.replies[0].userUpvoted}
+                            upvoteScore={props.replies[0].upvoteScore}
+                        />
+                    </div>
+                }
             </div>
         </div>
     )
